3d6f9dc30a2be0f08c7adf58908385125465eff6
Vivek Singh Rathore <kakashi@Viveks-MacBook-Pro.local>
2023-03-10 13:14:24 +0530

    write test case

--- test/ex_projecto_web/controllers/page_controller_test.exs
+++ test/ex_projecto_web/controllers/page_controller_test.exs
@@ -1,8 +1,25 @@
 defmodule ExProjectoWeb.PageControllerTest do
   use ExProjectoWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  alias ExProjecto.Users
+
+  describe "get users" do
+    setup [:create_users]
+
+    test "GET /", %{conn: conn} do
+      conn = get(conn, ~p"/")
+      assert %{"timestamp" => _, "users" => _} = json_response(conn, 200)
+    end
+  end
+
+  defp create_users(_) do
+    for _i <- 1..100 do
+      user = Users.create()
+    end
+
+    {:ok, state} = ExProjecto.init(%{})
+    # :timer.sleep(60 * 1000)
+    users = Users.get_all(state.min_number)
+    %{users: users}
   end
 end
